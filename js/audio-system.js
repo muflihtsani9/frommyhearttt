829a7808512ed89983d50c99287afd4ad3b4d5e0
muflihtsani9 <muflihtsani9@gmail.com>
2026-06-05 10:30:56 +0700

    Update audio-system.js

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'you’re gonna live forever in me',
+    artist:    'john mayer',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
